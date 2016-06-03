226f4d07cad22e88ae84f820d7137ebbb9f7d006
a10a <antoine.guillot4@free.fr>
2016-06-04 00:44:18 +0200

    Revert "erreur"
    This reverts commit f996460e7a24b4636fe8cddd021a5506408b4006.

--- application/controllers/Historique.php
+++ application/controllers/Historique.php
@@ -4,21 +4,7 @@ class Historique extends CI_Controller
 {      
         public function c1()
         {
-        	$sess_array=$this->session->userdata('logged_in');
-        	$data = array();
-			if($sess_array['username']!=NULL)
-                {
-                    $data['log_or_not']='<a href="'.  base_url() .'accueil/logout">Se déconnecter</a></li>';
-                }
-            else 
-                {
-                    $data['log_or_not']='Se connecter';
-                }
-                
-            $data['url_base'] = base_url();
-            $this->load->view('v_header', $data);
-	        $this->load->view('v_FestESAIP', $data);
-	        $this->load->view('v_footer');
+            echo 'LOL';
             
         }
 }

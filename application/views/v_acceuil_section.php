fb447d0fdda181616aba555bdd2d5ae0e483579f
a10a <antoine.guillot4@free.fr>
2016-06-04 18:09:17 +0200

    Bug polices
    Bug fixé.

---
+++ application/views/v_acceuil_section.php
@@ -0,0 +1,65 @@
+<?php
+/** Pied de page commun à l'ensemble du site
+ * 
+ * @author Adrien Pironneau
+ * @date 03/06/2016
+ * @version 0.2
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+
+
+		<section>
+			<aside id="slider_artistes">
+				<!-- Sources : http://codepen.io/dudleystorey/pen/ehKpi
+							  https://la-cascade.io/un-carrousel-responsif-en-pur-css/ -->
+				<h3>Artistes :</h3>
+				<figure id="artistes">
+					<p style="color:white; font-family:nirvana_font">NIRVANA</p>
+					<p style="color:white; font-family:ironmaiden_font">Iron Maiden</p>
+					<p style="color:white; font-family:acdc_font">AC*DC</p>
+					<p style="color:white; font-family:daftpunk_font">Daft Punk</p>
+					<p style="color:white; font-family:thebeatles_font">The Beatles</p>
+					<p style="color:white; font-family:scorpions_font">SCORPIONS</p>
+					<p style="color:white; font-family:nirvana_font">NIRVANA</p>
+					<p style="color:white; font-family:ironmaiden_font">Iron Maiden</p>
+					<p style="color:white; font-family:acdc_font">AC*DC</p>
+				</figure>
+			</aside>
+			<div>
+				<h3>Présentation :</h3>
+				<p>Bienvenue sur le site de Fest'Esaip, festival numéro 1 de Saint-Barthélémy
+				d'Anjou et ses environs. De nombreux artistes plus ou mois talentueux se 
+				succéderont lors de cette 42<sup>ème</sup> édition de Fest'Esaip sponsorisée 
+				de très grandes marques de renom tel que "Canard WC". Alors n'hésitez plus. Venez
+				participer à cet évènement unique, dans la joie et la bonne humeur, en achetant vos 
+				place grâce à l'onglet Blablablablablablablablablablablablablablablabla</p>
+			</div>
+			<aside id="slider_sponsors">
+				<!-- Sources : http://codepen.io/dudleystorey/pen/ehKpi
+							  https://la-cascade.io/un-carrousel-responsif-en-pur-css/
+				-->
+				<h3>Sponsors :</h3>
+				<figure id="sponsors">
+
+
+
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/sopalin.png')?>" "alt="Sopalin"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/carglass.png')?>" alt="Carglass"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/justinbridou.png')?>" alt="Justin Bridou"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/lapotagere.png')?>" alt="La Potagère"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/ticketrestaurant.png')?>" alt="Ticket Restaurant"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/canardwc.png')?>" alt="Canard WC"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/sopalin.png')?>" alt="Sopalin"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/carglass.png')?>" alt="Carglass"/>
+					<img class="small_image" src="<?php echo($url_base.'/assets/img/images/sponsors/justinbridou.png')?>" alt="Justin Bridou"/>
+				</figure>
+			</aside>
+		</section>
+</div><!-- fin de la div container -->
+
+
+</body>
+</html>
